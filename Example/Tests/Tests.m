72449e25c9e7bcc2e1b5f4f18a7ffe9cadfee4a2
Shaps Mohsenin <shaps@theappbusiness.com>
2014-11-16 22:59:43 +0000

    Updated provisioning and remote default failing tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -8,35 +8,7 @@
 
 SPEC_BEGIN(InitialTests)
 
-describe(@"My initial tests", ^{
-
-  context(@"will fail", ^{
-
-      it(@"can do maths", ^{
-          [[@1 should] equal:@2];
-      });
-
-      it(@"can read", ^{
-          [[@"number" should] equal:@"string"];
-      });
-    
-      it(@"will wait and fail", ^{
-          NSObject *object = [[NSObject alloc] init];
-          [[expectFutureValue(object) shouldEventually] receive:@selector(autoContentAccessingProxy)];
-      });
-  });
-
-  context(@"will pass", ^{
-    
-      it(@"can do maths", ^{
-        [[@1 should] beLessThan:@23];
-      });
-    
-      it(@"can read", ^{
-          [[@"team" shouldNot] containString:@"I"];
-      });  
-  });
-  
+describe(@"Email Validator", ^{  
 });
 
 SPEC_END
