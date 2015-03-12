a10ed39a560c4c718b5f35502a14cc7764744885
Shaps Mohsenin <shaps@theappbusiness.com>
2015-03-12 17:47:45 +0000

    Initial commit

---
+++ Example/SPXDataValidators/SPXViewController.m
@@ -0,0 +1,29 @@
+//
+//  SPXViewController.m
+//  SPXDataValidators
+//
+//  Created by Shaps Mohsenin on 03/12/2015.
+//  Copyright (c) 2014 Shaps Mohsenin. All rights reserved.
+//
+
+#import "SPXViewController.h"
+
+@interface SPXViewController ()
+
+@end
+
+@implementation SPXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
