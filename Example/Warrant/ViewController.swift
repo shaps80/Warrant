24ef2b8071020e5e136835d9c5a95a8bd27cda44
Shaps <shapsuk@me.com>
2016-02-15 11:59:12 +0000

    Files renamed

--- Example/Warrant/ViewController.swift
+++ Example/Warrant/ViewController.swift
@@ -11,58 +11,16 @@ import Warrant
 
 class ViewController: UITableViewController, UITextFieldDelegate {
   
-  deinit {
-    NSNotificationCenter.defaultCenter().removeObserver(self)
-  }
+  @IBOutlet var signInCell: SignInCell!
   
-  @IBOutlet var emailField: UITextField! {
-    didSet {
-      NSNotificationCenter.defaultCenter().addObserver(self, selector: "textFieldDidChange:", name: UITextFieldTextDidChangeNotification, object: emailField)
-    }
-  }
-  
-  @IBOutlet var passwordField: UITextField! {
-    didSet {
-      NSNotificationCenter.defaultCenter().addObserver(self, selector: "textFieldDidChange:", name: UITextFieldTextDidChangeNotification, object: passwordField)
-    }
-  }
-  
-  @IBOutlet var signInCell: SignInCell! {
-    didSet {
-      validate()
-    }
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    let validator = BlockValidator { (value) -> Void in
-      if let value = value as? String {
-        if value == "123" {
-          throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "123 is invalid"])
-        }
-      }
-    }
-
-    _ = try? validator.validate("123")
-    
-    validate()
+  func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+    _ = try? signInCell.validate()
+    return true
   }
   
   override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
     tableView.deselectRowAtIndexPath(indexPath, animated: true)
   }
-  
-  func validate() {
-    do {
-      try signInCell.validate()
-    } catch {
-      print(error)
-    }
-  }
-  
-  func textFieldDidChange(sender: UITextField?) {
-    validate()
-  }
+
   
 }
